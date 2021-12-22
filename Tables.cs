3e7d43de233b4ba7825f00920019ef547f7ea2af
Alex <voenkov.alex@gmail.com>
2021-12-22 11:26:49 +0300

    Функционал добавления строк

--- Tables.cs
+++ Tables.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Npgsql;
+using NpgsqlTypes;
 
 namespace PractikaDB
 {
@@ -27,7 +28,7 @@ namespace PractikaDB
             _connection = connection;
         }
 
-        #region Tables
+        #region TablesLoad
         private void Tables_Load(object sender, EventArgs e)
         {
             this.Left = left + width / 2 - (this.Width / 2);
@@ -74,10 +75,139 @@ namespace PractikaDB
             LoadData("couple");
         }
 
-        #endregion Tables
+        #endregion TablesLoad
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                if (_connection.State != ConnectionState.Open) _connection.Open();
 
+                switch (activeTabel)
+                {
+                    case "ankete":
+                    {
+                            for (int i = 0; i < dataGridView1.SelectedRows.Count; i++)
+                            {
+                                var row = dataGridView1.SelectedRows[i].Cells;
+                                var command = 
+                                    $"INSERT INTO ankete (name, surname, middlename, bday, phone, email, photo, workplace, position, compensation, isincouple, idphisical, idtest) " +
+                                    $"VALUES (@name, @surname, @middlename, @bday, @phone, @email, @photo, @workplace, @position, @compensation, @isincouple, @idphisical, @idtest)";
+
+                                var cmd = new NpgsqlCommand(command, _connection);
+                                cmd.Parameters.Add("@name", NpgsqlDbType.Varchar).Value = row["name"].Value;
+                                cmd.Parameters.Add("@surname", NpgsqlDbType.Varchar).Value = row["surname"].Value;
+                                cmd.Parameters.Add("@middlename", NpgsqlDbType.Varchar).Value = row["middlename"].Value;
+                                cmd.Parameters.Add("@bday", NpgsqlDbType.Date).Value = row["bday"].Value;
+                                cmd.Parameters.Add("@phone", NpgsqlDbType.Bigint).Value = row["phone"].Value;
+                                cmd.Parameters.Add("@email", NpgsqlDbType.Varchar).Value = row["email"].Value;
+                                cmd.Parameters.Add("@photo", NpgsqlDbType.Varchar).Value = row["photo"].Value;
+                                cmd.Parameters.Add("@workplace", NpgsqlDbType.Varchar).Value = row["workplace"].Value;
+                                cmd.Parameters.Add("@position", NpgsqlDbType.Varchar).Value = row["position"].Value;
+                                cmd.Parameters.Add("@compensation", NpgsqlDbType.Integer).Value = row["compensation"].Value;
+                                cmd.Parameters.Add("@isincouple", NpgsqlDbType.Boolean).Value = row["isincouple"].Value;
+                                cmd.Parameters.Add("@idphisical", NpgsqlDbType.Integer).Value = row["idphisical"].Value;
+                                cmd.Parameters.Add("@idtest", NpgsqlDbType.Integer).Value = row["idtest"].Value;
+                                try
+                                {
+                                    cmd.ExecuteNonQuery();
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.ToString());
+                                    return;
+                                }                                
+                            }
+
+                            MessageBox.Show("Строкы успешно добавлены!", "Успешно");
+                            LoadData(activeTabel);
+
+                            return;
+                    }
+                    case "phisical":
+                    {
+                            for (int i = 0; i < dataGridView1.SelectedRows.Count; i++)
+                            {
+                                var row = dataGridView1.SelectedRows[i].Cells;
+                                var command =
+                                    $"INSERT INTO phisical (Idankete, Haircolor, Weight, Height, IMT, Disease) " +
+                                    $"VALUES (@Idankete, @Haircolor, @Weight, @Height, @IMT, @Disease)";
+
+                                var cmd = new NpgsqlCommand(command, _connection);
+                                cmd.Parameters.Add("@Idankete", NpgsqlDbType.Integer).Value = row["Idankete"].Value;
+                                cmd.Parameters.Add("@Haircolor", NpgsqlDbType.Unknown).Value = row["Haircolor"].Value;
+                                cmd.Parameters.Add("@Weight", NpgsqlDbType.Integer).Value = row["Weight"].Value;
+                                cmd.Parameters.Add("@Height", NpgsqlDbType.Integer).Value = row["Height"].Value;
+                                cmd.Parameters.Add("@IMT", NpgsqlDbType.Numeric).Value = Convert.ToInt32(row["Weight"].Value) / (Convert.ToInt32(row["Height"].Value) * Convert.ToInt32(row["Height"].Value));
+                                cmd.Parameters.Add("@Disease", NpgsqlDbType.Boolean).Value = row["Disease"].Value;
+
+                                try
+                                {
+                                    cmd.ExecuteNonQuery();
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.ToString());
+                                    return;
+                                }
+                            }
+
+                            MessageBox.Show("Строкы успешно добавлены!", "Успешно");
+                            LoadData(activeTabel);
+
+                            return;
+                        }
+                    case "test":
+                        {
+                            for (int i = 0; i < dataGridView1.SelectedRows.Count; i++)
+                            {
+                                var row = dataGridView1.SelectedRows[i].Cells;
+                                var command =
+                                    $"INSERT INTO test (Idankete, Score, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16) " +
+                                    $"VALUES (@Idankete, @Score, @q1, @q2, @q3, @q4, @q5, @q6, @q7, @q8, @q9, @q10, @q11, @q12, @q13, @q14, @q15, @q16)";
+
+                                var cmd = new NpgsqlCommand(command, _connection);
+                                cmd.Parameters.Add("@Idankete", NpgsqlDbType.Integer).Value = row["Idankete"].Value;
+                                cmd.Parameters.Add("@Score", NpgsqlDbType.Integer).Value = row["Score"].Value;
+                                cmd.Parameters.Add("@q1", NpgsqlDbType.Boolean).Value = row["q1"].Value;
+                                cmd.Parameters.Add("@q2", NpgsqlDbType.Boolean).Value = row["q2"].Value;
+                                cmd.Parameters.Add("@q3", NpgsqlDbType.Boolean).Value = row["q3"].Value;
+                                cmd.Parameters.Add("@q4", NpgsqlDbType.Boolean).Value = row["q4"].Value;
+                                cmd.Parameters.Add("@q5", NpgsqlDbType.Boolean).Value = row["q5"].Value;
+                                cmd.Parameters.Add("@q6", NpgsqlDbType.Boolean).Value = row["q6"].Value;
+                                cmd.Parameters.Add("@q7", NpgsqlDbType.Boolean).Value = row["q7"].Value;
+                                cmd.Parameters.Add("@q8", NpgsqlDbType.Boolean).Value = row["q8"].Value;
+                                cmd.Parameters.Add("@q9", NpgsqlDbType.Boolean).Value = row["q9"].Value;
+                                cmd.Parameters.Add("@q10", NpgsqlDbType.Boolean).Value = row["q10"].Value;
+                                cmd.Parameters.Add("@q11", NpgsqlDbType.Boolean).Value = row["q11"].Value;
+                                cmd.Parameters.Add("@q12", NpgsqlDbType.Boolean).Value = row["q12"].Value;
+                                cmd.Parameters.Add("@q13", NpgsqlDbType.Boolean).Value = row["q13"].Value;
+                                cmd.Parameters.Add("@q14", NpgsqlDbType.Boolean).Value = row["q14"].Value;
+                                cmd.Parameters.Add("@q15", NpgsqlDbType.Boolean).Value = row["q15"].Value;
+                                cmd.Parameters.Add("@q16", NpgsqlDbType.Boolean).Value = row["q16"].Value;
+
+                                try
+                                {
+                                    cmd.ExecuteNonQuery();
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.ToString());
+                                    return;
+                                }
+                            }
+
+                            MessageBox.Show("Строкы успешно добавлены!", "Успешно");
+                            LoadData(activeTabel);
+
+                            return;
+                        }
+                    case "couple":
+                        {
+                            return;
+                        }
+                    default: return;
+                }
+            }
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
